761cc5453a30d967502bb22bcb53bc3ca86af3b7
Francesco Primerano <francescoprimerano@Air-di-Angela.home-life.hub>
2021-08-19 20:52:27 +0200

    first commit

---
+++ src/Modal.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import SweetAlert from "react-bootstrap-sweetalert";
+import "bulma/css/bulma.min.css";
+import { Content, Button } from "react-bulma-components";
+
+function Modal({ fieldsModal, setFieldsModal }) {
+  console.log(fieldsModal);
+  const { show } = fieldsModal;
+  const [showModal, setShowModal] = useState(show);
+
+  const handlerClick = (e) => {
+    e.preventDefault();
+    setShowModal("false");
+    setFieldsModal(undefined);
+  };
+
+  return (
+    <div>
+      <SweetAlert
+        show={showModal === "true" ? true : false}
+        success={fieldsModal.success === "true" ? true : false}
+        danger={fieldsModal.danger === "true" ? true : false}
+        title={fieldsModal.title}
+        showConfirm={false}
+        onConfirm={() => {}}
+      >
+        <Content>
+          <p>{fieldsModal.body}</p>
+          <Button
+            onClick={handlerClick}
+            fullwidth={true}
+            color={fieldsModal.btnType}
+          >
+            OK
+          </Button>
+        </Content>
+      </SweetAlert>
+    </div>
+  );
+}
+
+export default Modal;
